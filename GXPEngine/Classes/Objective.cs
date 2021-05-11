12c527d07aeae1ab2ce3b046214cbc6e6e6f95e6
Zaftan <63283934+Zaftan@users.noreply.github.com>
2021-05-11 22:45:57 +0200

    rocket animated

--- GXPEngine/Classes/Objective.cs
+++ GXPEngine/Classes/Objective.cs
@@ -18,29 +18,34 @@ class Objective : PhysicsRectangle
     public Objective(int width, int height, int x, int y, string destination) : base(width, height, x, y)
     {
         trigger = true;
-        SetColor(System.Drawing.Color.Green);
+        //SetColor(System.Drawing.Color.Green);
         dest = destination;
 
         rocket = ((Level2)game.Currentscene).rocket;
-        rocket.SetCycle(1, 18);
     }
 
     public override void Update()
     {
         base.Update();
+
         if (deadTimer != null)
         {
+            rocket.Animate();
             if (rocket.y >= 100)
             {
                 rocket.y--;
             }
 
-            //if(rocket.y <= 100)
-            //{
-            //    LateRemove();
-            //    game.SceneManager.GotoNextscene();
-            //}
-            
+            if(rocket.currentFrame >= 11)
+            {
+                rocket.SetCycle(13, 18);
+            }
+
+            if (rocket.y <= 100)
+            {
+                LateRemove();
+                game.SceneManager.GotoNextscene();
+            }
         }
     }
 
@@ -48,11 +53,11 @@ class Objective : PhysicsRectangle
     {
         if (other is Ball)
         {
-            //((Ball)other).Die();
+            ((Ball)other).Die();
 
             if (deadTimer == null)
             {
-                deadTimer = new Timer(10.0f);
+                deadTimer = new Timer(0.1f);
                 
                 AddChild(deadTimer);
             }
